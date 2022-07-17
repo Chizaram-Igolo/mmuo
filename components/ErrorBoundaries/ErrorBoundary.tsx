445f3228d14d159ca751c3b20ce977afb27a1bef
Chizaram-Igolo <chizaram@yahoo.com>
2022-07-17 12:19:40 +0100

    🩹 Fix typings in ErrorBoundary

--- components/ErrorBoundaries/ErrorBoundary.tsx
+++ components/ErrorBoundaries/ErrorBoundary.tsx
@@ -1,44 +1,56 @@
-import React from 'react';
+import React, { ErrorInfo } from "react";
 
-class ErrorBoundary extends React.Component {
-    constructor(props) {
-      super(props)
-  
-      // Define a state variable to track whether is an error or not
-      this.state = { hasError: false }
-    }
-    static getDerivedStateFromError(error) {
-      // Update state so the next render will show the fallback UI
-  
-      return { hasError: true }
-    }
-    componentDidCatch(error, errorInfo) {
-      // You can use your own error logging service here
-      console.log({ error, errorInfo })
-    }
-    render() {
-      // Check if the error is thrown
-      if (this.state.hasError) {
-        // You can render any custom fallback UI 
+interface Props {
+  children: React.ReactNode;
+}
 
+interface State {
+  hasError: boolean;
+}
 
-        return (
-          <div>
-            <h2>Oops, there is an error!</h2>
-            <button
-              type="button"
-              onClick={() => this.setState({ hasError: false })}
-            >
-              Try again?
-            </button>
-          </div>
-        )
-      }
-  
-      // Return children components in case of no error
-  
-      return this.props.children
-    }
+class ErrorBoundary extends React.Component<Props, State> {
+  // Define a state variable to track whether is an error or not
+  public state = {
+    hasError: false,
+  };
+
+  constructor(props: Props) {
+    super(props);
   }
-  
-  export default ErrorBoundary;
+
+  public static getDerivedStateFromError(_: Error) {
+    // Update state so the next render will show the fallback UI
+
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can use your own error logging service here
+    console.log("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    // Check if the error is thrown
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try again?
+          </button>
+        </div>
+      );
+    }
+
+    // Return children components in case of no error
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
